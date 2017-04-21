2f4e140f7789af26601cf7c1b9faeabe1d87aa66
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2017-04-20 19:39:27 -0500

    Setup Lock Screen

---
+++ keg_login/tests/test_middleware.py
@@ -0,0 +1,17 @@
+import keg_login.middleware as middleware
+
+
+class TestLockedOutMiddleware(object):
+    def test_excluded_routes(self):
+        cls = middleware.LockedOutMiddleware
+        # No Session Key
+        assert not cls().should_lock_out('thing', {})
+
+        # In default excluded
+        assert not cls().should_lock_out('static', {'keg-login.lockout': True})
+
+        # In extra routes
+        assert not cls(exclude=['thingy']).should_lock_out('thingy', {'keg-login.lockout': True})
+
+        # Sad path
+        assert cls().should_lock_out('thingy', {'keg-login.lockout': True})
